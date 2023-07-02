be682fac954789a721471fd33b3240dd2eab3997
Dmitriy Kozak <dmitrij.kozak.1997@bk.ru>
2023-07-02 15:05:37 +0400

    MvnAutoTest added

---
+++ src/main/java/BonusService.java
@@ -0,0 +1,12 @@
+public class BonusService {
+
+    public long calculate(long amount, boolean isRegistered) {
+        int percent = isRegistered ? 3 : 1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        return bonus;
+    }
+}
